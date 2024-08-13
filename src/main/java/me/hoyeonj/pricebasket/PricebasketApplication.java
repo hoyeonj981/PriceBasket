d4756e228883bbaf7bcc62646b2e891ec968c9ac
hoyeonj981 <hoyeonj981@gmail.com>
2024-08-13 15:29:06 +0900

    init

---
+++ src/main/java/me/hoyeonj/pricebasket/PricebasketApplication.java
@@ -0,0 +1,13 @@
+package me.hoyeonj.pricebasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricebasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricebasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hoyeonj/pricebasket/PricebasketApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hoyeonj.pricebasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricebasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
