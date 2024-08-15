4cba875cdb4653bd0f4105a492a4d8be18d530c8
hoyeonj981 <hoyeonj981@gmail.com>
2024-08-15 13:49:34 +0900

    style: google java style에 맞게 수정

--- src/main/java/me/hoyeonj/pricebasket/PricebasketApplication.java
+++ src/main/java/me/hoyeonj/pricebasket/PricebasketApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PricebasketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PricebasketApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(PricebasketApplication.class, args);
+  }
 }
