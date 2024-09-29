9941b56b7332d971ff0b0f829bb82e2e7625a012
hoyeonj981 <hoyeonj981@gmail.com>
2024-09-29 23:56:43 +0900

    refactor: DTO 객체 record로 변경

--- src/main/java/me/hoyeonj/pricebasket/adapter/out/EmartMallHtmlFetcher.java
+++ src/main/java/me/hoyeonj/pricebasket/adapter/out/EmartMallHtmlFetcher.java
@@ -13,18 +13,18 @@ public class EmartMallHtmlFetcher {
   public HtmlDocument fetchFrom(final EmartMallSearchQuery query) {
     var uri = SEARCH_URL.createSearchUrl(query.query());
     var responseDocument = client.fetchFromUri(uri);
-    if (!responseDocument.getContent().contains("<html>")) {
+    if (!responseDocument.content().contains("<html>")) {
       throw new NotHtmlDocumentException("해당 uri에 대한 응답이 html 형식이 아닙니다. - "
-          + responseDocument.getUrl());
+          + responseDocument.url());
     }
-    if (responseDocument.getStatusCode() == 404) {
+    if (responseDocument.statusCode() == 404) {
       throw new PageNotFoundException("해당 uri에 대한 응답 페이지를 찾을 수 없습니다. - "
-          + responseDocument.getUrl());
+          + responseDocument.url());
     }
-    if (responseDocument.getStatusCode() == 500) {
+    if (responseDocument.statusCode() == 500) {
       throw new InternalServerErrorException("해당 uri에 대한 요청을 서버에서 응답하지 않습니다. - "
-          + responseDocument.getUrl());
+          + responseDocument.url());
     }
-    return new HtmlDocument(responseDocument.getContent());
+    return new HtmlDocument(responseDocument.content());
   }
 }
--- src/main/java/me/hoyeonj/pricebasket/adapter/out/ResponseDocument.java
+++ src/main/java/me/hoyeonj/pricebasket/adapter/out/ResponseDocument.java
@@ -1,37 +1,11 @@
 package me.hoyeonj.pricebasket.adapter.out;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-public class ResponseDocument {
-
-  private final String url;
-  private final int statusCode;
-  private final Map<String, List<String>> headers;
-  private final String content;
-
-  public ResponseDocument(final String url, final int statusCode,
-      final Map<String, List<String>> headers, final String content) {
-    this.url = url;
-    this.statusCode = statusCode;
-    this.headers = headers;
-    this.content = content;
-  }
-
-  public String getUrl() {
-    return url;
-  }
-
-  public int getStatusCode() {
-    return statusCode;
-  }
-
-  public Map<String, List<String>> getHeaders() {
-    return Collections.unmodifiableMap(headers);
-  }
-
-  public String getContent() {
-    return content;
-  }
-}
+public record ResponseDocument(
+  String url,
+  int statusCode,
+  Map<String, List<String>> headers,
+  String content
+) { }
