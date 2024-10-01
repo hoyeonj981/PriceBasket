c8101623ace0eb991ee5aa93b88ae61f10953d47
hoyeonj981 <hoyeonj981@gmail.com>
2024-10-01 14:45:52 +0900

    style: 잘못된 스타일 수정

--- src/main/java/me/hoyeonj/pricebasket/adapter/out/ItemNameNotFoundException.java
+++ src/main/java/me/hoyeonj/pricebasket/adapter/out/ItemNameNotFoundException.java
@@ -1,6 +1,6 @@
 package me.hoyeonj.pricebasket.adapter.out;
 
-public class ItemNameNotFoundException extends RuntimeException{
+public class ItemNameNotFoundException extends RuntimeException {
 
   public ItemNameNotFoundException(final String s) {
     super(s);
