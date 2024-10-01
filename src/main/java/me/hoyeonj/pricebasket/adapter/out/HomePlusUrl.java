e65d2796ef2e9f6cc451c147e6301f5ce4ef3f2c
hoyeonj981 <hoyeonj981@gmail.com>
2024-10-01 18:27:50 +0900

    feat: homeplus url 생성 기능 구현

---
+++ src/main/java/me/hoyeonj/pricebasket/adapter/out/HomePlusUrl.java
@@ -0,0 +1,67 @@
+package me.hoyeonj.pricebasket.adapter.out;
+
+import java.util.Objects;
+
+enum HomePlusUrl {
+
+  HTTPS_SEARCH_URL(
+      Protocol.HTTPS,
+      "mfront.homeplus.co.kr",
+      "search",
+      "TD",
+      "entry=direct"
+  );
+
+  private static final String KEYWORD_PARAMETER = "inputKeyword";
+  private static final String MALLTYPE_PARAMETER = "mallType";
+  private static final String PATH_DELIMETER = "/";
+  private static final String QUERY_DELIMETER = "?";
+  private static final String VALUE_ASSIGNER = "=";
+  private static final String PARAMETER_SEPARATOR = "&";
+
+  private final Protocol protocol;
+  private final String domain;
+  private final String path;
+  private final String mallType;
+  private final String defaultParameter;
+
+  HomePlusUrl(final Protocol protocol, final String domain, final String path,
+      final String mallType, final String defaultParameter) {
+    this.protocol = protocol;
+    this.domain = domain;
+    this.path = path;
+    this.mallType = mallType;
+    this.defaultParameter = defaultParameter;
+  }
+
+  // https://mfront.homeplus.co.kr/search?entry=direct&inputKeyword=%EA%B0%90%EC%9E%90&mallType=TD
+  public String createSearchUrl(final String queryParameterValue) {
+    validateQueryParameter(queryParameterValue);
+
+    final var builder = new StringBuilder();
+    builder.append(this.protocol.getValue());
+    builder.append(this.domain);
+    builder.append(this.PATH_DELIMETER);
+    builder.append(this.path);
+    builder.append(this.QUERY_DELIMETER);
+    builder.append(this.defaultParameter);
+    builder.append(this.QUERY_DELIMETER);
+    builder.append(this.PARAMETER_SEPARATOR);
+    builder.append(this.KEYWORD_PARAMETER);
+    builder.append(this.VALUE_ASSIGNER);
+    builder.append(queryParameterValue);
+    builder.append(this.QUERY_DELIMETER);
+    builder.append(this.MALLTYPE_PARAMETER);
+    builder.append(this.VALUE_ASSIGNER);
+    builder.append(this.mallType);
+
+    return builder.toString();
+  }
+
+  private void validateQueryParameter(final String queryParameterValue) {
+    if (Objects.isNull(queryParameterValue) || queryParameterValue.isBlank()) {
+      throw new InvalidQueryParameterException("유요하지 않는 homeplus 쿼리 파라미터입니다. - "
+          + queryParameterValue);
+    }
+  }
+}
