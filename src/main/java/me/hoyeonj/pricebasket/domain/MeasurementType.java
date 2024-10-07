dda0c27e6d73c7e1f0984d15128df6423bc15c01
hoyeonj981 <hoyeonj981@gmail.com>
2024-10-08 04:43:40 +0900

    refactor: 기본 단위 계산 메서드 제거. 기본 미터법 변환 기능 추가

--- src/main/java/me/hoyeonj/pricebasket/domain/MeasurementType.java
+++ src/main/java/me/hoyeonj/pricebasket/domain/MeasurementType.java
@@ -1,25 +1,26 @@
 package me.hoyeonj.pricebasket.domain;
 
-import static me.hoyeonj.pricebasket.domain.MeasurementType.StandardUnitMultipliers.G_TO_100G;
-import static me.hoyeonj.pricebasket.domain.MeasurementType.StandardUnitMultipliers.KG_TO_100G;
-import static me.hoyeonj.pricebasket.domain.MeasurementType.StandardUnitMultipliers.L_TO_100ML;
-import static me.hoyeonj.pricebasket.domain.MeasurementType.StandardUnitMultipliers.ML_TO_100ML;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 
 public enum MeasurementType {
 
-  G("g", G_TO_100G),
-  KG("kg", KG_TO_100G),
-  ML("ml", ML_TO_100ML),
-  L("l", L_TO_100ML)
+  G("g", BigDecimal::new),
+  KG("kg", MeasurementType::getDecimal),
+  ML("ml", BigDecimal::new),
+  L("l", MeasurementType::getDecimal),
   ;
 
-  private static final int PRECISION = 10;
+  private static BigDecimal getDecimal(final String amount) {
+    return new BigDecimal(amount)
+        .multiply(BigDecimal.valueOf(1000L))
+        .setScale(0, RoundingMode.HALF_UP);
+  }
+
   private static final Map<String, MeasurementType> symbolMap = new HashMap<>();
 
   static {
@@ -29,7 +30,7 @@ public enum MeasurementType {
   }
 
   private final String symbol;
-  private final BigDecimal standardMultiplier;
+  private final Function<String, BigDecimal> metricUnitConverter;
 
   public static MeasurementType from(final String symbol) {
     if (Objects.isNull(symbol)) {
@@ -38,35 +39,22 @@ public enum MeasurementType {
     final var lowerSymbol = symbol.toLowerCase();
     final var measurementType = symbolMap.get(lowerSymbol);
     if (Objects.isNull(measurementType)) {
-      throw new IllegalArgumentException("해당 표현 단위는 존재하지 않습니다. - " + symbol);
+      throw new NotSupportedUnitException("해당 표현 단위는 존재하지 않습니다. - " + symbol);
     }
     return measurementType;
   }
 
-  MeasurementType(final String symbol, final BigDecimal standardMultiplier) {
+  MeasurementType(final String symbol, final Function<String, BigDecimal> metricUnitConverter) {
     this.symbol = symbol;
-    this.standardMultiplier = standardMultiplier;
+    this.metricUnitConverter = metricUnitConverter;
   }
 
-  /*
-  * g, ml : (unit price) = 100 / (amout) * (total price)
-  * kg, l : (unit price) = 0.1 / (amount) * (total price)
-   */
-  public PriceWon calculateStandardUnitPrice(final PriceWon totalPrice, final BigDecimal amount) {
-    final BigDecimal divided = standardMultiplier.divide(amount, PRECISION, RoundingMode.HALF_DOWN);
-    final var value = totalPrice.getValue();
-    final var multiplied = divided.multiply(value);
-    return PriceWon.of(multiplied);
+  public BigDecimal convertToBasicUnit(final String amount) {
+    return symbolMap.get(this.symbol)
+        .metricUnitConverter.apply(amount);
   }
 
   public String getSymbol() {
-    return "100" + this.symbol;
-  }
-
-  static class StandardUnitMultipliers {
-    static BigDecimal ML_TO_100ML = new BigDecimal("100");
-    static BigDecimal L_TO_100ML = new BigDecimal("0.1");
-    static BigDecimal G_TO_100G = new BigDecimal("100");
-    static BigDecimal KG_TO_100G = new BigDecimal("0.1");
+    return this.symbol;
   }
 }
--- src/test/java/me/hoyeonj/pricebasket/domain/MeasurementTypeTest.java
+++ src/test/java/me/hoyeonj/pricebasket/domain/MeasurementTypeTest.java
@@ -5,47 +5,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.math.BigDecimal;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MeasurementTypeTest {
 
-  @DisplayName("단위 Symbol이 null이거나 존재하지 않는다면 예외가 발생한다")
+  @DisplayName("단위 Symbol이 존재하지 않는다면 예외가 발생한다")
   @ParameterizedTest
-  @MethodSource("invalidSymbols")
-  void throw_exception_when_symbol_is_null_or_not_exist(String given) {
+  @ValueSource(strings = {
+      "", " ",
+      "oz", "lb", "t", "gz"
+  })
+  void throw_exception_when_symbol_is_not_exist(String given) {
+    assertThatThrownBy(() -> from(given))
+        .isInstanceOf(NotSupportedUnitException.class);
+  }
+
+  @DisplayName("단위 Symbol이 null이라면 예외가 발생한다")
+  @Test
+  void throw_exception_when_symbol_is_null() {
+    String given = null;
     assertThatThrownBy(() -> from(given))
         .isInstanceOf(IllegalArgumentException.class);
   }
 
-  private static Stream<String> invalidSymbols() {
-    return Stream.of(
-        null, "", " ",
-        "oz", "lb", "t", "gz"
-    );
-  }
-
-  @DisplayName("단위당 가격은 100을 기준으로 계산된다")
+  @DisplayName("주어진 양을 기본 미터법으로 변환한다")
   @ParameterizedTest
   @CsvSource({
-      "1.8, l, 2680, 149",
-      "3.6, L, 5360, 149",
-      "1, KG, 3780, 378",
-      "1, Kg, 1980, 198",
-      "315, G, 5980, 1899",
-      "1000, g, 4880, 488"
+      "l, 1.5, 1500",
+      "ml, 355, 355",
+      "g, 1500, 1500",
+      "kg, 3.9, 3900",
   })
-  void unit_of_price_is_calculated_on_the_basis_100(
-      String amount, String symbol, String totalPrice, String unitPrice) {
-    var givenAmount = new BigDecimal(amount);
-    var givenTotalPrice = PriceWon.of(totalPrice);
-    var expected = PriceWon.of(unitPrice);
+  void convert_amount_to_basic_metric(
+      String givenSymbol, String givenAmount, String givenExpected) {
+    var symbol = from(givenSymbol);
+    var expected = new BigDecimal(givenExpected);
 
-    var actual = from(symbol)
-        .calculateStandardUnitPrice(givenTotalPrice, givenAmount);
+    var actual = symbol.convertToBasicUnit(givenAmount);
 
     assertThat(actual).isEqualTo(expected);
   }
