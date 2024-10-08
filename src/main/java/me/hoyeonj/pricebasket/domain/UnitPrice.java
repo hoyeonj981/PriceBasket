5da2bd49eeb727356e16adc7215d35b5589633d5
hoyeonj981 <hoyeonj981@gmail.com>
2024-10-08 19:50:05 +0900

    feat: 단위가격 클래스 구현

--- src/main/java/me/hoyeonj/pricebasket/domain/UnitPrice.java
+++ src/main/java/me/hoyeonj/pricebasket/domain/UnitPrice.java
@@ -1,53 +1,76 @@
 package me.hoyeonj.pricebasket.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
-public class UnitPrice {
+public class UnitPrice implements Comparable<UnitPrice> {
 
   private final MeasurementType unit;
   private final PriceWon priceWon;
+  private final BigDecimal totalAmount;
+  private final UnitPriceCalculator calculator;
 
-  public static UnitPrice create(final BigDecimal amount, final PriceWon totalPrice,
-      final String unitSymbol) {
-    checkZeroAmount(amount);
-    checkZeroPrice(totalPrice);
-    final var unit = getMeasurementType(unitSymbol);
-    final var unitOfPrice = unit.calculateStandardUnitPrice(totalPrice, amount);
-    return new UnitPrice(unit, unitOfPrice);
+  public static UnitPrice create(final BigDecimal totalAmount, final PriceWon totalPrice,
+      final String unitSymbol, final UnitPriceCalculator calculator) {
+    final var unitPrice = calculator.calculate(totalPrice, totalAmount, unitSymbol);
+    return new UnitPrice(
+        MeasurementType.from(unitSymbol),
+        unitPrice,
+        totalAmount,
+        calculator
+    );
   }
 
-  private static void checkZeroPrice(final PriceWon priceWon) {
-    if (priceWon.compareTo(PriceWon.ZERO) == 0) {
-      throw new ZeroTotalPriceException("총가격은 0일 수 없습니다");
-    }
-  }
-
-  private static void checkZeroAmount(final BigDecimal amount) {
-    if (amount.compareTo(BigDecimal.ZERO) == 0) {
-      throw new ZeroAmountException("단위 가격에서 양은 0일 수 없습니다");
-    }
-  }
-
-  private static MeasurementType getMeasurementType(final String unitSymbol) {
-    MeasurementType unit;
-    try {
-      unit = MeasurementType.from(unitSymbol);
-    } catch (IllegalArgumentException e) {
-      throw new NotSupportedUnitException(e.getMessage());
-    }
-    return unit;
-  }
-
-  private UnitPrice(final MeasurementType unit, final PriceWon priceWon) {
+  private UnitPrice(final MeasurementType unit, final PriceWon priceWon,
+      final BigDecimal totalAmount, final UnitPriceCalculator calculator) {
     this.unit = unit;
     this.priceWon = priceWon;
+    this.totalAmount = totalAmount;
+    this.calculator = calculator;
+  }
+
+  public UnitPrice updateUnitPrice(final PriceWon totalPrice) {
+    return new UnitPrice(
+        this.unit,
+        calculator.calculate(totalPrice, this.totalAmount, this.unit.getSymbol()),
+        this.totalAmount,
+        this.calculator
+    );
   }
 
   public PriceWon getPriceWon() {
     return PriceWon.of(priceWon.getValue());
   }
 
-  public String getUnit() {
-    return unit.getSymbol();
+  public MeasurementType getUnit() {
+    return this.unit;
+  }
+
+  public BigDecimal getTotalAmount() {
+    return BigDecimal.valueOf(this.totalAmount.longValue());
+  }
+
+  @Override
+  public int compareTo(final UnitPrice other) {
+    return this.priceWon.compareTo(other.priceWon);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final UnitPrice unitPrice = (UnitPrice) o;
+    return unit == unitPrice.unit && Objects.equals(priceWon, unitPrice.priceWon)
+        && Objects.equals(totalAmount, unitPrice.totalAmount) && Objects.equals(
+        calculator, unitPrice.calculator);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(unit, priceWon, totalAmount, calculator);
   }
 }
--- src/test/java/me/hoyeonj/pricebasket/domain/UnitPriceTest.java
+++ src/test/java/me/hoyeonj/pricebasket/domain/UnitPriceTest.java
@@ -1,71 +1,73 @@
 package me.hoyeonj.pricebasket.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
-import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class UnitPriceTest {
 
-  @DisplayName("주어진 양, 단위, 총가격, 100 단위가격을 올바르게 계산한다")
+  @Mock
+  private UnitPriceCalculator mockCalculator;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @DisplayName("정상적으로 UnitPrice 객체를 생성한다")
+  @Test
+  void create_unit_price_object() {
+    when(mockCalculator.calculate(any(), any(), any()))
+        .thenReturn(PriceWon.of(1000));
+
+    var actual = UnitPrice.create(BigDecimal.TEN, PriceWon.of(5000), "G", mockCalculator);
+
+    assertThat(actual.getUnit()).isEqualTo(MeasurementType.G);
+    assertThat(actual.getPriceWon()).isEqualTo(PriceWon.of(1000));
+    assertThat(actual.getTotalAmount()).isEqualTo(BigDecimal.TEN);
+  }
+
+  @DisplayName("총 가격이 변경되면 새롭게 단위가격을 생성한다")
+  @Test
+  void updating_will_create_new_unit_price_object() {
+    var givenPrice = 2000L;
+    when(mockCalculator.calculate(any(), any(), any()))
+        .thenReturn(PriceWon.of(1000))
+        .thenReturn(PriceWon.of(givenPrice));
+    var expected = PriceWon.of(givenPrice);
+
+    var unitPrice = UnitPrice.create(BigDecimal.TEN, PriceWon.of(5000), "G", mockCalculator);
+    var updatedUnitPrice = unitPrice.updateUnitPrice(PriceWon.of(10000));
+    var actual = updatedUnitPrice.getPriceWon();
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @DisplayName("단위 가격은 가격으로 비교한다")
   @ParameterizedTest
   @CsvSource({
-      "100, g, 10_000, 10_000",
-      "2, kg, 10_000, 500",
-      "500, ml, 10_000, 2_000",
-      "5, l, 10_000, 200",
-      "2.5, kg, 24_900, 996",
+     "100, 200, -1",
+      "100, 100, 0",
+      "200, 100, 1",
   })
-  void create_should_calculate_correct_unitPrice(
-      BigDecimal amount, String givenSymbol, long total, long expectedUnitPrice) {
-    var totalPrice = PriceWon.of(total);
-    var expectedPrice = PriceWon.of(expectedUnitPrice);
-    var expectedSymbol = "100" + givenSymbol;
+  void compare_unit_price_correctly(String left, String right, int expected) {
+    when(mockCalculator.calculate(any(), any(), any()))
+        .thenReturn(PriceWon.of(left))
+        .thenReturn(PriceWon.of(right));
 
-    var actual = UnitPrice.create(amount, totalPrice, givenSymbol);
-    var symbolUnit = actual.getUnit();
+    var unitPrice1 = UnitPrice.create(BigDecimal.TEN, PriceWon.of(5000), "G", mockCalculator);
+    var unitPrice2 = UnitPrice.create(BigDecimal.TEN, PriceWon.of(10000), "G", mockCalculator);
+    var actual = unitPrice1.compareTo(unitPrice2);
 
-    assertThat(actual.getPriceWon()).isEqualTo(expectedPrice);
-    assertThat(symbolUnit).isEqualTo(expectedSymbol);
-  }
-
-  @DisplayName("지원하지 않는 단위 표기는 예외가 발생한다")
-  @ParameterizedTest
-  @MethodSource("invalidSymbols")
-  void throw_exception_when_unit_symbol_is_not_supported(String givenSymbol) {
-    var amount = new BigDecimal("100");
-    var totalPrice = PriceWon.of("100");
-
-    assertThatThrownBy(() -> UnitPrice.create(amount, totalPrice, givenSymbol))
-        .isInstanceOf(NotSupportedUnitException.class);
-  }
-
-  private static Stream<String> invalidSymbols() {
-    return Stream.of(
-        null, "", " ",
-        "oz", "lb", "t", "gz"
-    );
-  }
-
-  @DisplayName("양이 0일 경우 예외가 발생한다")
-  @Test
-  void throw_exception_when_zero_amount() {
-    PriceWon total = PriceWon.of(10_000L);
-    assertThatThrownBy(() -> UnitPrice.create(BigDecimal.ZERO, total, "g"))
-        .isInstanceOf(ZeroAmountException.class);
-  }
-
-  @DisplayName("총가격이 0일 경우 예외가 발생한다")
-  @Test
-  void throw_exception_when_total_price_is_zero() {
-    PriceWon total = PriceWon.of(BigDecimal.ZERO);
-   assertThatThrownBy(() -> UnitPrice.create(BigDecimal.TEN, total, "g"))
-       .isInstanceOf(ZeroTotalPriceException.class);
+    assertThat(actual).isEqualTo(expected);
   }
 }
