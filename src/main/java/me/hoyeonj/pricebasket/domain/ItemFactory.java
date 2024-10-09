e35f2b9fd433d03728345d8bfb47712a7b299b86
hoyeonj981 <hoyeonj981@gmail.com>
2024-10-09 21:17:10 +0900

    feat: 상품 팩토리 클래스 구현

---
+++ src/main/java/me/hoyeonj/pricebasket/domain/ItemFactory.java
@@ -0,0 +1,31 @@
+package me.hoyeonj.pricebasket.domain;
+
+public class ItemFactory {
+
+  private final UnitPriceCalculator calculator;
+
+  public ItemFactory(final UnitPriceCalculator calculator) {
+    this.calculator = calculator;
+  }
+
+  public Item createEmarMallItem(ItemFactoryInput input) {
+    return createFromData(input, MallType.EMARTMALL);
+  }
+
+  public Item createHomeplusItem(ItemFactoryInput input) {
+    return createFromData(input, MallType.HOMEPLUS);
+  }
+
+  private Item createFromData(ItemFactoryInput input, MallType mallType) {
+    final var totalPrice = PriceWon.of(input.totalPrice());
+    final var unitPrice = UnitPrice.create(input.totalAmount(), totalPrice, input.unitSymbol(),
+        calculator);
+    final var detailsUrl = new ItemUrl(input.detailsUrl());
+    final var imageUrl = new ItemUrl(input.imageUrl());
+
+    final var itemInfo = new ItemInfo(input.name(), totalPrice, unitPrice, input.rating(),
+        detailsUrl, imageUrl);
+
+    return Item.withoutId(itemInfo, mallType);
+  }
+}
