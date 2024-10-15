cb214e5ee1e8767968cf02642bd66a58f23a523e
hoyeonj981 <hoyeonj981@gmail.com>
2024-10-15 21:06:02 +0900

    feat: 카테고리 id 기능 구현

---
+++ src/main/java/me/hoyeonj/pricebasket/domain/CategoryId.java
@@ -0,0 +1,42 @@
+package me.hoyeonj.pricebasket.domain;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CategoryId {
+
+  private final String value;
+
+  static CategoryId create() {
+    return new CategoryId(UUID.randomUUID().toString());
+  }
+
+  static CategoryId from(final String id) {
+    return new CategoryId(id);
+  }
+
+  private CategoryId(final String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final CategoryId that = (CategoryId) o;
+    return Objects.equals(value, that.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value);
+  }
+}
