e0d2f83830abb09d23041cf4a7bcc48f462a8179
hoyeonj981 <hoyeonj981@gmail.com>
2024-10-16 18:35:32 +0900

    feat: password 기능 구현

---
+++ src/main/java/me/hoyeonj/pricebasket/domain/ClientPassword.java
@@ -0,0 +1,39 @@
+package me.hoyeonj.pricebasket.domain;
+
+import java.util.Objects;
+
+public class ClientPassword {
+
+  private final String value;
+
+  public static ClientPassword create(final String rawPassword, final PasswordEncoder encoder,
+      final PasswordValidator validator){
+    validator.validate(rawPassword);
+    return new ClientPassword(encoder.encode(rawPassword));
+  }
+
+  private ClientPassword(final String value) {
+    this.value = value;
+  }
+
+  public String getPassword() {
+    return value;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final ClientPassword that = (ClientPassword) o;
+    return Objects.equals(value, that.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(value);
+  }
+}
---
+++ src/test/java/me/hoyeonj/pricebasket/domain/ClientPasswordTest.java
@@ -0,0 +1,65 @@
+package me.hoyeonj.pricebasket.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ClientPasswordTest {
+
+  @Mock
+  private PasswordEncoder encoder;
+
+  @Mock
+  private PasswordValidator validator;
+
+  @DisplayName("비밀번호를 생성한다")
+  @Test
+  void createPasswordObject() {
+    var givenPassword = "validPassword";
+    var hashedPassword = "hashedPassword";
+    when(encoder.encode(anyString())).thenReturn(hashedPassword);
+    doNothing().when(validator).validate(givenPassword);
+
+    var actual = ClientPassword.create(givenPassword, encoder, validator);
+
+    assertThat(actual.getPassword()).isEqualTo(hashedPassword);
+  }
+
+  @DisplayName("비밀번호 값이 같다면 같은 객체이다")
+  @Test
+  void sameObjectWhenPasswordValueIsSame() {
+    var givenPassword1 = "validPassword1";
+    var givenPassword2 = "validPassword2";
+    var hashedPassword = "hashedPassword";
+    when(encoder.encode(anyString())).thenReturn(hashedPassword);
+    doNothing().when(validator).validate(givenPassword1);
+
+    var password1 = ClientPassword.create(givenPassword1, encoder, validator);
+    var password2 = ClientPassword.create(givenPassword2, encoder, validator);
+
+    assertThat(password1).isEqualTo(password2);
+  }
+
+  @DisplayName("비밀번호가 값이 같다면 hashcode 값도 같다")
+  @Test
+  void sameHashCodeWhenPasswordValueIsSame() {
+    var givenPassword1 = "validPassword1";
+    var givenPassword2 = "validPassword2";
+    var hashedPassword = "hashedPassword";
+    when(encoder.encode(anyString())).thenReturn(hashedPassword);
+    doNothing().when(validator).validate(givenPassword1);
+
+    var password1 = ClientPassword.create(givenPassword1, encoder, validator);
+    var password2 = ClientPassword.create(givenPassword2, encoder, validator);
+
+    assertThat(password1.hashCode()).isEqualTo(password2.hashCode());
+  }
+}
