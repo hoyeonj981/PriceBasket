f1587e23ef501ddd7497246287f44eb537769cc4
hoyeonj981 <hoyeonj981@gmail.com>
2024-11-15 14:48:37 +0900

    refactor: basketResult 결과값 변경

--- src/main/java/me/hoyeonj/pricebasket/application/in/dto/BasketResult.java
+++ src/main/java/me/hoyeonj/pricebasket/application/in/dto/BasketResult.java
@@ -1,9 +1,11 @@
 package me.hoyeonj.pricebasket.application.in.dto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public record BasketResult(
     List<BasketItem> items,
-    String totalPrice
+    BigDecimal totalPrice,
+    int totalItemsCount
 ) {
 }
