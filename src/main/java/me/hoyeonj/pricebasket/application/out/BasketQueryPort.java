ca0a83e338a035a1621f8ba02b705bfaba90046e
hoyeonj981 <hoyeonj981@gmail.com>
2024-11-15 14:49:12 +0900

    feat: 전체 조회 기능 추가

--- src/main/java/me/hoyeonj/pricebasket/application/out/BasketQueryPort.java
+++ src/main/java/me/hoyeonj/pricebasket/application/out/BasketQueryPort.java
@@ -1,10 +1,14 @@
 package me.hoyeonj.pricebasket.application.out;
 
+import java.util.List;
 import java.util.Optional;
 import me.hoyeonj.pricebasket.domain.Basket;
 import me.hoyeonj.pricebasket.domain.BasketId;
+import me.hoyeonj.pricebasket.domain.ClientId;
 
 public interface BasketQueryPort {
 
   Optional<Basket> findById(BasketId basketId);
+
+  List<Basket> findAll(ClientId domainClientId);
 }
