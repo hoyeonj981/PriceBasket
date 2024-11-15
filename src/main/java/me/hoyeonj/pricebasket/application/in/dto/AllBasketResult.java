8ea78b6595049e0d2e1fb8eafca8b7affab3210f
hoyeonj981 <hoyeonj981@gmail.com>
2024-11-16 04:19:29 +0900

    feat: 컬렉션 복사본 전달 getter 구현

--- src/main/java/me/hoyeonj/pricebasket/application/in/dto/AllBasketResult.java
+++ src/main/java/me/hoyeonj/pricebasket/application/in/dto/AllBasketResult.java
@@ -1,9 +1,13 @@
 package me.hoyeonj.pricebasket.application.in.dto;
 
+import java.util.Collections;
 import java.util.List;
 
 public record AllBasketResult(
     List<BasketSummary> values
 ) {
 
+  public List<BasketSummary> values() {
+    return Collections.unmodifiableList(values);
+  }
 }
--- src/main/java/me/hoyeonj/pricebasket/application/in/dto/BasketResult.java
+++ src/main/java/me/hoyeonj/pricebasket/application/in/dto/BasketResult.java
@@ -1,11 +1,17 @@
 package me.hoyeonj.pricebasket.application.in.dto;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 
 public record BasketResult(
     List<BasketItem> items,
     BigDecimal totalPrice,
+    String martName,
     int totalItemsCount
 ) {
+
+  public List<BasketItem> items() {
+    return Collections.unmodifiableList(items);
+  }
 }
