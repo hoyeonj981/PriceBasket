0fd0d806d38aafc66a15b4a5e28fb3b8109b8146
hoyeonj981 <hoyeonj981@gmail.com>
2024-11-15 14:50:02 +0900

    refactor: 전체 상품 정보 조회에서 상품id, 수량만 조회하도록 변경

--- src/main/java/me/hoyeonj/pricebasket/application/in/dto/BasketItem.java
+++ src/main/java/me/hoyeonj/pricebasket/application/in/dto/BasketItem.java
@@ -1,14 +1,8 @@
 package me.hoyeonj.pricebasket.application.in.dto;
 
 public record BasketItem(
-    String name,
-    String price,
-    String unitPrice,
-    String amount,
-    String unitSymbol,
-    String rating,
-    String detailsUrl,
-    String imageUrl
+    String itemId,
+    int quantity
 ) {
 
 }
